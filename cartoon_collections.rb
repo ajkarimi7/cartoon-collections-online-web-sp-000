50fc5eac44372ce8f64acf39a8eb39556c92c64d
Ali Karimi <ajkarimi7@gmail.com>
2019-09-10 19:22:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,6 +17,6 @@ def find_the_cheese(potentially_cheesy_items)
   cheese_types = ["cheddar", "gouda", "camembert"]
   
   potentially_cheesy_items.find do |maybe_cheese|
-    cheeses.include(maybe_cheese)
+    cheeses.include?(maybe_cheese)
   end
 end
